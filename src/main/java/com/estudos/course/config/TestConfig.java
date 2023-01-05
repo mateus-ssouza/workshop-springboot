aa78644dfa57efcf64942712fb84bd669edde317
Mateus Souza <msouzacrv@gmail.com>
2023-01-05 08:42:15 -0300

    Category

--- src/main/java/com/estudos/course/config/TestConfig.java
+++ src/main/java/com/estudos/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.estudos.course.config;
 
+import com.estudos.course.entities.Category;
 import com.estudos.course.entities.Order;
 import com.estudos.course.entities.User;
 import com.estudos.course.entities.enums.OrderStatus;
+import com.estudos.course.repository.CategoryRepository;
 import com.estudos.course.repository.OrderRepository;
 import com.estudos.course.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +25,18 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
         userRepository.saveAll(Arrays.asList(u1, u2));
